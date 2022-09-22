deb15a445aa6c7313b3b225b696bbacdb663b531
quangquan4179 <quangquan4179@gmail.com>
2022-09-22 14:12:57 +0700

    Initial commit

---
+++ test/simple-cdk-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleCdkEc2 from '../lib/simple-cdk-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-cdk-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleCdkEc2.SimpleCdkEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
