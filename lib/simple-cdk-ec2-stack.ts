4b27ea09b79069ff517ddce42f263c16dc190bc4
quangquan4179 <quangquan4179@gmail.com>
2022-09-22 18:32:07 +0700

    add file

--- lib/simple-cdk-ec2-stack.ts
+++ lib/simple-cdk-ec2-stack.ts
@@ -1,16 +1,72 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+// import * as cdk from 'aws-cdk-lib';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from '@aws-cdk/aws-ec2'
+import * as iam from '@aws-cdk/aws-iam'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SimpleCdkEc2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id,props);
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      subnetConfiguration: [
+        {name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC},
+      ],
+    });
 
-    // The code that defines your stack goes here
+    // 👇 create Security Group for the Instance
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleCdkEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    // 👇 create a Role for the EC2 Instance
+    const webserverRole = new iam.Role(this, 'webserver-role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
+      ],
+    });
+
+    // 👇 create the EC2 Instance
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance-sdk', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      role: webserverRole,
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: 'training-ec2-key',
+    });
+    new cdk.CfnOutput(this, 'simple-instance-1-output', {
+      value: ec2Instance.instancePublicIp
+    })
+
   }
 }
